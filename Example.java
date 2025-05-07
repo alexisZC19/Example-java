b0fa5bf083df78d121d364c7b0c8953aba48ab75
alexisZC19 <126813702+alexisZC19@users.noreply.github.com>
2025-05-07 17:38:07 -0600

    Add files via upload
    creation of the example

---
+++ Example.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("La suma de " + a + " y " + b + " es: " + suma);
+    }
+}
+
